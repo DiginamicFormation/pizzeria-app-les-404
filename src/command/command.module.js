d25a844ad00b69c806cadb84c45afa34bca27d8d
Damien <dguironnet@gmail.com>
2017-08-28 16:42:34 +0200

    item.service

--- src/command/command.module.js
+++ src/command/command.module.js
@@ -1,4 +1,9 @@
+import ItemService from "../item/item.service"
+import ApiUrlsService from "../apiUrls/apiUrls.service"
+
 const commandModule = angular
     .module('commandModule', [])
+    .service('itemService', ItemService)
+    .value('apiUrlsService', ApiUrlsService)
 
 export default commandModule
