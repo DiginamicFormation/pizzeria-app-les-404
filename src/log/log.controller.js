fcfa5da628bdc059aad91777103bf6698e749f7d
etbourreau <et.bourreau@laposte.net>
2017-08-28 17:14:07 +0200

    temp

--- src/log/log.controller.js
+++ src/log/log.controller.js
@@ -2,6 +2,12 @@ export default class logCtrl{
     constructor(logService){
         this.logService = logService
     }
+    
+    tryConnect(email, password){
+        if(this.logService.checkUser(email, password)){
+            this.logService.connect()
+        }
+    }
 }
 
 logCtrl.$inject = ['logService'];
