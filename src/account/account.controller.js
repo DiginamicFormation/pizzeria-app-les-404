01ecdd0cdfd46143f499e0f5b5018600dadf5245
Damien <dguironnet@gmail.com>
2017-08-29 16:07:35 +0200

    personnal info account

--- src/account/account.controller.js
+++ src/account/account.controller.js
@@ -1,14 +1,24 @@
 export default class AccountController {
-    constructor() {
+    constructor($http, accountService) {
+        this.accountService = accountService
+
         this.master = {};
         this.reset();
+
+        accountService.getUserById(sessionStorage.getItem('userId', true)).then((user) => {
+            this.user = user
+            this.master = angular.copy(user)
+        })
     }
 
     update(user) {
-        this.master = angular.copy(user);
+        this.master = angular.copy(user)
+        this.accountService.updateUser(user)
     }
 
     reset() {
-        this.user = angular.copy(this.master);
+        this.user = angular.copy(this.master)
     }
 }
+
+AccountController.$inject = ['$http', 'accountService']
