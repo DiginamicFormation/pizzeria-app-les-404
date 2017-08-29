ed528a2ac87c66869cadc90e945799136a3e3bdf
Damien <dguironnet@gmail.com>
2017-08-29 15:44:11 +0200

    account page - start

--- src/account/account.controller.js
+++ src/account/account.controller.js
@@ -1,5 +1,14 @@
 export default class AccountController {
     constructor() {
-        
+        this.master = {};
+        this.reset();
+    }
+
+    update(user) {
+        this.master = angular.copy(user);
+    }
+
+    reset() {
+        this.user = angular.copy(this.master);
     }
 }
