b8ed5d5025bffae62d4067fdef18ff187a9dcc06
Damien <dguironnet@gmail.com>
2017-08-29 12:40:21 +0200

    add / remove actions in items list pages

--- src/menu/itemsList.controller.js
+++ src/menu/itemsList.controller.js
@@ -1,14 +1,26 @@
 export default class ItemsListCtrl {
-    constructor(itemService,  $location){
+    constructor(itemService, $location) {
         this.itemService = itemService
         this.$location = $location
         this.itemType = this.$location.hash()
 
         console.log("hash:", this.$location.hash());
 
-        itemService.getItemByType(this.itemType).then((result) => {
+        itemService.getItemsByType(this.itemType).then((result) => {
             this.itemsList = result
         })
+    }
+
+    getItemsList() {
+        return this.itemsList
+    }
+
+    addToBasket(idItem, quantity = 1) {
+        console.log("Add to basket: idItem:", idItem, "/ qty:", quantity);
+    }
+
+    removeFromBasket(idItem, quantity = 1) {
+        console.log("Remove from basket: idItem:", idItem, "/ qty:", quantity);
     }
 }
 
