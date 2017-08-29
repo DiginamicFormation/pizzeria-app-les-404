1bce247ece12bc7b2025ceef2527a080b208c483
Damien <dguironnet@gmail.com>
2017-08-29 10:28:35 +0200

    pizzas list page

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import command from './command/command.module'
 import menu from './menu/menu.module'
 
 angular
-  .module('pizzeriaApp', [uiBootstrap, log.name])
+  .module('pizzeriaApp', [uiBootstrap, log.name, menu.name])
   .config(($routeProvider) => {
       $routeProvider.otherwise({
           redirectTo : '/'
--- src/menu/menu.module.js
+++ src/menu/menu.module.js
@@ -1,7 +1,25 @@
+import ngRoute from 'angular-route'
+
+// Services
 import ItemService from "../item/item.service"
+import ApiUrlsService from "../apiUrls/apiUrls.service"
+
+// Components
+import pizzasComponent from "./pizzas.component"
 
 const menuModule = angular
-    .module('menuModule', [])
+    .module('menuModule', [ngRoute])
+
+    .component('pizzasComponent', pizzasComponent)
+
     .service('itemService', ItemService)
+    .value('apiUrlsService', ApiUrlsService)
+
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/pizzas', {
+                template: '<pizzas-component></pizzas-component>'
+            })
+    }])
 
 export default menuModule
