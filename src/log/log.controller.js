f4911dc7c8882b09fe9830824972631e80653b7f
etbourreau <et.bourreau@laposte.net>
2017-08-30 11:58:34 +0200

    connexion id fix and minor bugs

--- src/log/log.controller.js
+++ src/log/log.controller.js
@@ -2,14 +2,13 @@ export default class logCtrl{
     constructor(logService, $location){
         this.logService = logService
         this.$location = $location
-        console.log(logService);
     }
     
     tryConnect(email, password){
         this.logService.checkUser(email, password)
             .then(found => {
                 if(found){
-                    this.logService.connect(found)
+                    this.logService.connect(found.id)
                     this.$location.path('/')
                     this.$location.hash('')
                 }else{
